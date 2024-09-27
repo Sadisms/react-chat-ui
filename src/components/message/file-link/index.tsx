06e880987314ae5ebd137f3e7fbe97e91755a709
дмитрий Madara <madara@deeplay.io>
2024-09-27 16:56:40 +0600

    Edit file component

--- src/components/message/file-link/index.tsx
+++ src/components/message/file-link/index.tsx
@@ -1,4 +1,25 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const FileContainer = styled.a`
+text-align:left;
+vertical-align:text-top;
+font-size:14px;
+align-self:flex-start;
+line-height:auto;
+color:#000000;
+font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+padding-left:16px;
+padding-right:16px;
+padding-top:8px;
+padding-bottom:8px;
+position: relative;
+box-sizing: border-box;
+word-wrap: break-word;
+width: 100%;
+text-decoration: none;
+user-select: none;
+`
 
 interface FileDownloadLinkProps {
   fileUrl: string;
@@ -39,13 +60,13 @@ const FileDownloadLink: React.FC<FileDownloadLinkProps> = ({ fileUrl, fileName ,
 
   return (
     <div>
-      <a
+      <FileContainer
         href={fileUrl}
         onClick={downloadFile}
         style={{ pointerEvents: loading ? 'none' : 'auto', textDecoration: 'underline', color: loading ? 'gray' : 'blue' }}
       >
         {loading ? 'Loading...' : fileName}
-      </a>
+      </FileContainer>
       {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
   );
