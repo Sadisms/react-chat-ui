28343b85962e57750364518c5f5ba57bebfe9d4f
дмитрий Madara <madara@deeplay.io>
2024-10-01 17:32:03 +0600

    Edit file component

--- src/components/message/file-link/index.tsx
+++ src/components/message/file-link/index.tsx
@@ -21,6 +21,18 @@ text-decoration: none;
 user-select: none;
 `
 
+const DownloadIcon = <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="none"
+    viewBox="0 0 24 24"
+    style={{ position: "absolute", left: 12, top: 8 }}
+    strokeWidth="2"
+    stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+</svg>
+
 interface FileDownloadLinkProps {
   fileUrl: string;
   fileName: string;
@@ -58,14 +70,18 @@ const FileDownloadLink: React.FC<FileDownloadLinkProps> = ({ fileUrl, fileName ,
     }
   };
 
+  let text = <>DownloadIcon&nbsp;&nbsp;&nbsp;&nbsp;<span style={{ textDecoration: "underline" }}>{fileName}</span></>
+  if (loading)
+    text = <>Loading...</>;
+
   return (
     <div>
       <FileContainer
         href={fileUrl}
         onClick={downloadFile}
-        style={{ pointerEvents: loading ? 'none' : 'auto', textDecoration: 'underline' }}
+        style={{ pointerEvents: loading ? 'none' : 'auto', textDecoration: 'underline', color: loading ? 'gray' : 'blue' }}
       >
-        {loading ? 'Loading...' : fileName}
+        {text}
       </FileContainer>
       {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
