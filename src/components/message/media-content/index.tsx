ce69662e6a48202721912724631709c118bbde33
дмитрий Madara <madara@deeplay.io>
2024-10-03 15:21:32 +0600

    Edit file component

--- src/components/message/media-content/index.tsx
+++ src/components/message/media-content/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 import { MediaType } from '../../../types/MessageType'
 import { getBorderCss } from '../borderController'
-import FileDownloadLink from "../file-link";
 
 
 interface Props extends MediaType {
@@ -19,6 +18,38 @@ const ImageContainer = styled.div`
 user-select: none;
 
     `
+
+const FileContainer = styled.a`
+text-align:left;
+vertical-align:text-top;
+font-size:14px;
+align-self:flex-start;
+line-height:auto;
+color:#000000;
+font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+padding-left:16px;
+padding-right:16px;
+padding-top:8px;
+padding-bottom:8px;
+position: relative;
+box-sizing: border-box;
+word-wrap: break-word;
+width: 100%;
+text-decoration: none;
+user-select: none;
+`
+
+const DownloadIcon = <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="none"
+    viewBox="0 0 24 24"
+    style={{ position: "absolute", left: 12, top: 8 }}
+    strokeWidth="2"
+    stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+</svg>
 
 const Image = styled.img<{
     borderCss: string,
@@ -91,11 +122,9 @@ export default function MediaContent({
                         </Video>
                     }
                     <div style={{ width: "100%", display: 'flex' }}>
-                        <FileDownloadLink
-                            fileUrl={url}
-                            fileName={name || url}
-                            axiosClient={axiosClient}
-                        />
+                        <FileContainer
+                            target='_blank'
+                            href={url}>{DownloadIcon}&nbsp;&nbsp;&nbsp;&nbsp;<span style={{ textDecoration: "underline" }}>{url}</span></FileContainer>
                     </div>
                 </div>
 
