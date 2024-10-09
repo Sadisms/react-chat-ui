7209e8d744d0edebefe44c6e636793c16ee3139f
дмитрий Madara <madara@deeplay.io>
2024-10-09 11:37:58 +0600

    Update loading component

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,48 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
 import './index.css';
-import useColorSet from '../../hooks/useColorSet';
 
-type Props = {
-  themeColor?: string;
-};
-
-const Container = styled.div`
-  height: 100%;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-sizing: border-box;
-  position: relative;
-`;
-
-const InternalDiv = styled.div<{ themeColor?: string }>`
-  box-sizing: border-box;
-  width: 42px;
-  height: 42px;
-  margin: 8px;
-  position: absolute;
-  border: 6px solid #fff;
-  border-radius: 50%;
-  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-  border-color: ${({ themeColor }) => themeColor} transparent transparent
-    transparent;
-  box-sizing: border-box;
-`;
-
-export default function Loading({ themeColor }: Props) {
-
-  const color = useColorSet("--loader-color")
+const Loading = ({ size = 50, color = '#3498db' }) => {
+  const loaderStyle = {
+    width: size,
+    height: size,
+    border: `4px solid ${color}`,
+    borderTop: `4px solid transparent`,
+  };
 
   return (
-    <Container>
-      <div className="lds-ring">
-        <InternalDiv themeColor={color || themeColor} />
-        <InternalDiv themeColor={color || themeColor} />
-        <InternalDiv themeColor={color || themeColor} />
-        <InternalDiv themeColor={color || themeColor} />
-      </div>
-    </Container>
+    <div className="loader-wrapper">
+      <div className="loader" style={loaderStyle}></div>
+    </div>
   );
-}
+};
+
+export default Loading;
